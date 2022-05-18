6785cf1a89d256f6c485f9027174a34eddad13cc
john@johnhart96.co.uk <john@johnhart96.co.uk>
2022-05-18 10:31:56 +0100

    Adding support for bind errors

--- inc/bind.php
+++ inc/bind.php
@@ -2,4 +2,7 @@
 $ds = ldap_connect( LDAP_SERVER );
 ldap_set_option( $ds, LDAP_OPT_PROTOCOL_VERSION, 3 );
 $bind = ldap_bind( $ds , LDAP_ADMINUSER , LDAP_ADMINPASSWD );
+if( ! $bind ) {
+    die( "Error: Unable to bind to LDAP, please check your settings and retry!" );
+}
 ?>
