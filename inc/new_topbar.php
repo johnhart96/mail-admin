144c2425882ec359cd0e2d4728952ab51a0d432e
John Hart <john@jh96.co.uk>
2024-12-13 11:53:48 +0000

    Adding icons to the menu

--- inc/new_topbar.php
+++ inc/new_topbar.php
@@ -6,8 +6,8 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="<?php echo APP_MAIL; ?>"><strong>Mail</strong></a></li>
-                <li class="nav-item"><a class="nav-link" href="<?php echo APP_DRIVE; ?>"><strong>Drive</strong></a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo APP_MAIL; ?>"><i class="fas fa-mail-bulk"></i>&nbsp;Mail</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo APP_DRIVE; ?>"><i class="fas fa-folder"></i>&nbsp;Drive</a></li>
                 <?php
                 function nav_item( $page , $link ) {
                     $current_page = str_replace( "/" , "" , $_SERVER['SCRIPT_NAME'] );
@@ -17,17 +17,17 @@
                         echo "<li class='nav-item'><a href='$link' class='nav-link'>$page</a></li>";
                     }
                 }
-            if( $_SESSION['admin_level'] !== "self" ) {
-                nav_item( "Domains" , "domains.php" );
-                nav_item( "Mailboxes" , "users.php" );
-                nav_item( "Groups" , "groups.php" );
-                nav_item( "Aliases" , "alias.php" );
-                if( $_SESSION['admin_level'] == "global" ) {
-                    nav_item( "Server" , "server.php" );
+                if( $_SESSION['admin_level'] !== "self" ) {
+                    nav_item( "<i class='fas fa-building'></i>&nbsp;Domains" , "domains.php" );
+                    nav_item( "<i class='fas fa-inbox'></i>&nbsp;Mailboxes" , "users.php" );
+                    nav_item( "<i class='fa fa-group'></i>&nbsp;Groups" , "groups.php" );
+                    nav_item( "<i class='fas fa-mask'></i>&nbsp;Aliases" , "alias.php" );
+                    if( $_SESSION['admin_level'] == "global" ) {
+                        nav_item( "<i class='fas fa-server'></i>&nbsp;Server" , "server.php" );
+                    }
                 }
-            }
-            nav_item( "Account" , "settings.php" );
-            ?>
+                nav_item( "<i class='fas fa-user-alt'></i>&nbsp;Account" , "settings.php" );
+                ?>
             </ul>
         </div>
     </div>
